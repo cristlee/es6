31f8dd5a847e5be331d8f31f5ab808a8f1d79e62
Crist Lee <cristlee@icloud.com>
2017-06-06 12:57:32 +0800

    rest and spread params

--- src/demo.ts
+++ src/demo.ts
@@ -6,6 +6,19 @@ for(let i=1; i<=10; i++){
 
 let getValue = function (value:number=10, bonus=5) {
     console.log(value+bonus)
+    console.log(arguments.length)
 }
 
 getValue(undefined, 6)
+
+let displayColors = function(message: string, ...colors:string[]){
+    console.log(message)
+    for(let i in colors){
+        console.log(i)
+        console.log(arguments)
+        console.log(colors[i])
+    }
+}
+
+let colors = ['red','green','blue']
+displayColors('message', ...colors)
