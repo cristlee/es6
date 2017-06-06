04b105c42e63a3682e522274235afb1dcd44c7b5
Crist Lee <cristlee@icloud.com>
2017-06-06 20:14:12 +0800

    add some demo

--- src/demo.ts
+++ src/demo.ts
@@ -65,5 +65,59 @@ console.log(lna2)
 let [fn3, ...other] = c
 console.log(fn3)
 console.log(other)
+let me = {
+    firstname: 'crist',
+    lastname: 'lee',
+    ages: 28
+}
+let {firstname: f, ages} = me
+console.log(f)
+console.log(ages)
 
+// string templetes
+let user = 'crist'
+let welcome = `welcome ${user}  home
+this is second line`
+console.log(welcome)
 
+// for of loops
+let letters: any = ['A', 'B', 'C']
+for(let letter of letters){
+    console.log(letter);
+}
+letters = 'CBA'
+for(let letter of letters){
+    console.log(letter);
+}
+
+// class and class inheritance
+class Car{ 
+    name: string;
+    drive() { console.log('car drive method');
+     10 }
+    constructor(name:string){
+        this.name = name
+        console.log(`car init method ${name}`)
+    }
+    static StaticMethod(){
+        console.log('static');
+    }
+}
+class Bus extends Car{
+    constructor(name:string){
+        super(name);
+        this.name = name + 'q'
+        console.log('bus init method');
+    }
+    StaticMethod(){
+        super.drive()
+        console.log('bus static method');
+    }
+}
+let car = new Car('crist')
+let bus = new Bus('lee')
+bus.StaticMethod()
+console.log(bus.name);
+console.log(car.drive === Car.prototype.drive);
+makecar()
+function makecar() {}
